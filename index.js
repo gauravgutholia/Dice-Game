cd8257c5db840bfb1ded177c8aa5885544d5fec4
Gaurav Gutholia <gauravgutholia7@gmail.com>
2020-06-29 02:57:35 -0400

    update 8.0
    emoji added

--- index.js
+++ index.js
@@ -22,58 +22,22 @@ function throwDice(btn) {
   //If player 1 wins
   if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🏆 Play 1 Wins!";
+     document.getElementsByClassName("angry")[1].style.visibility = "visible";
+    document.getElementsByClassName("angry1")[0].style.visibility = "visible";
+    document.getElementsByClassName("angry")[0].style.visibility = "hidden";
+    document.getElementsByClassName("angry1")[1].style.visibility = "hidden";
   } else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🏆";
+    document.getElementsByClassName("angry")[1].style.visibility = "hidden";
+    document.getElementsByClassName("angry1")[0].style.visibility = "hidden";
+    document.getElementsByClassName("angry")[0].style.visibility = "visible";
+    document.getElementsByClassName("angry1")[1].style.visibility = "visible";
   } else {
     document.querySelector("h1").innerHTML = "Draw! 🚩";
+     document.getElementsByClassName("angry")[1].style.visibility = "hidden";
+    document.getElementsByClassName("angry1")[0].style.visibility = "visible";
+    document.getElementsByClassName("angry")[0].style.visibility = "visible";
+    document.getElementsByClassName("angry1")[1].style.visibility = "hidden";
   }
 }
 
-// copy code
-/*document.getElementById("btn").addEventListener("click", throwDice);
-
-function throwDice(btn) {
-  // Dice 1 Settings
-
-  // location.reload();
-
-  var dice1No = Math.floor(Math.random() * 6) + 1;
-
-  console.log("Dice One No = " + dice1No);
-
-  var dice1img = document.querySelector(".img1");
-
-  dice1img.setAttribute("src", "images/dice" + dice1No + ".png");
-
-  // Dice 2 Settings
-
-  var dice2No = Math.floor(Math.random() * 6) + 1;
-
-  console.log("Dice Two No = " + dice2No);
-
-  var dice2img = document.querySelector(".img2");
-
-  dice2img.setAttribute("src", "images/dice" + dice2No + ".png");
-
-  // Change H1 Text According to Dice Numbers
-
-  /*
- 
-Three Results
- 
-1. Player 1 Win
-2. Player 2 Win 
-3. Draw
- 
-*/
-
-/* var h1Text = document.querySelector("h1");
-
-  if (dice1No > dice2No) {
-    h1Text.innerText = "Player 1 Win 🚩";
-  } else if (dice1No < dice2No) {
-    h1Text.innerText = "🚩 Player 2 Win ";
-  } else if (dice1No == dice2No) {
-    h1Text.innerText = "Draw! Try Again";
-  }
-}*/
